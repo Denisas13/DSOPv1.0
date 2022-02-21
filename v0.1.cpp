28b523655ba971ae7796d84c4c7d1bbd03cf44ef
Denisas13 <95296806+Denisas13@users.noreply.github.com>
2022-02-21 20:41:22 +0200

    Add files via upload

---
+++ v0.1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct student{
+string v, p;
+double ND[50], egz;
+};
+
+void Ivestis(int i, int n, student A[]);
+void Isvestis(student A[], int n, int kiek);
+
+int main(){
+
+student A[50];
+char yn;
+int n, kiek=0;
+
+cout << "Kiek buvo namu darbu? ";
+cin >> n;
+
+for(int i=0; i<50;i++){
+    cout << "Ar norite prideti studenta? y/n";
+    cin >> yn;
+    if(yn=='y'){
+        Ivestis(i,n,A);
+        kiek++;
+    }
+    else if(yn=='n'){
+        break;
+    }
+    else{
+        i--;
+    }
+}
+Isvestis(A,n,kiek);
+
+system("pause");
+return 0;
+}
+void Ivestis(int i, int n, student A[]){
+cout << "Iveskite studento varda: ";
+cin >> A[i].v;
+cout << "Iveskite studento pavarde: ";
+cin >> A[i].p;
+for(int j=0; j<n; j++){
+    cout << "Iveskite " << j+1 << "-ojo namu darbo ivertinima: ";
+    cin >> A[i].ND[j];
+}
+cout << "Iveskite egzamino ivertinima: ";
+cin >> A[i].egz;
+}
+void Isvestis(student A[], int n, int kiek){
+cout << setw(20) << "Vardas" << setw(20) << "Pavarde" << setw(15) << "Galutinis ivertinimas" << endl;
+cout << "----------------------------------------------" << endl;
+for(int i=0; i<kiek; i++){
+    cout << setw(20) << A[i].v << setw(20) << A[i].p << setw(15) << A[i].egz << endl;
+}
+}
