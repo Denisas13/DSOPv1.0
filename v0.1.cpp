14ea22cbe94b5c7dd23ac9b47c07e4685e7e6422
Denisas13 <95296806+Denisas13@users.noreply.github.com>
2022-02-21 22:26:27 +0200

    namų darbų kiekis nėra žinomas iš anksto

--- v0.1.cpp
+++ v0.1.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 struct student{
+int n;
 string v, p;
 double ND[50], egz, NDv, galutinis, mediana;
 };
 
-void Ivestis(int i, int n, student A[]);
-void Isvestis(student A[], int n, int kiek);
+void Ivestis(int i, student A[]);
+void Isvestis(student A[], int kiek, char vidmed);
 double NDvidurkis(double A[], int n);
 double Galutinis(double egz, double nd);
 double NDmediana(double A[], int n);
@@ -19,10 +20,7 @@ int main(){
 
 student A[50];
 char yn, vidmed;
-int n, kiek=0;
-
-cout << "Kiek buvo namu darbu? ";
-cin >> n;
+int kiek=0;
 
 do{
 cout << "Galutini ivertinima isvesti kaip vidurki, ar kaip mediana? (v/m): ";
@@ -34,11 +32,11 @@ for(int i=0; i<50;i++){
     cout << "Ar norite prideti studenta? (y/n): ";
     cin >> yn;
     if(yn=='y'){
-        Ivestis(i,n,A);
+        Ivestis(i,A);
         kiek++;
-        SortArray(A[i].ND,n);
-        A[i].NDv=NDvidurkis(A[i].ND,n);
-        A[i].mediana=NDmediana(A[i].ND,n);
+        SortArray(A[i].ND,A[i].n);
+        A[i].NDv=NDvidurkis(A[i].ND,A[i].n);
+        A[i].mediana=NDmediana(A[i].ND,A[i].n);
         if(vidmed=='v') A[i].galutinis=Galutinis(A[i].egz,A[i].NDv);
         else if(vidmed=='m') A[i].galutinis=Galutinis(A[i].egz,A[i].mediana);
     }
@@ -50,30 +48,34 @@ for(int i=0; i<50;i++){
     }
 }
 
-Isvestis(A,n,kiek);
+Isvestis(A,kiek,vidmed);
 
 system("pause");
 return 0;
 }
 
-void Ivestis(int i, int n, student A[]){
+void Ivestis(int i, student A[]){
 cout << "Iveskite studento varda: ";
 cin >> A[i].v;
 cout << "Iveskite studento pavarde: ";
 cin >> A[i].p;
-for(int j=0; j<n; j++){
-    cout << "Iveskite " << j+1 << "-ojo namu darbo ivertinima: ";
+A[i].n=0;
+for(int j=0; j<50; j++){
+    cout << "Iveskite " << j+1 << "-ojo namu darbo ivertinima (arba 0, jeigu jau ivesti visi ivertinimai): ";
     cin >> A[i].ND[j];
+    if(A[i].ND[j]==0)break;
+    A[i].n++;
 }
 cout << "Iveskite egzamino ivertinima: ";
 cin >> A[i].egz;
 }
 
-void Isvestis(student A[], int n, int kiek){
-cout << setw(20) << std::left << "Vardas" << std::left << setw(20) << "Pavarde" << setw(21)  << std::right << "Galutinis ivertinimas" << endl;
-cout << "-------------------------------------------------------------" << endl;
+void Isvestis(student A[],int kiek, char vidmed){
+if(vidmed=='v')cout << setw(20) << std::left << "Vardas" << std::left << setw(20) << "Pavarde" << setw(28)  << std::right << "Galutinis ivertinimas (vid.)" << endl;
+if(vidmed=='m')cout << setw(20) << std::left << "Vardas" << std::left << setw(20) << "Pavarde" << setw(28)  << std::right << "Galutinis ivertinimas (med.)" << endl;
+cout << "--------------------------------------------------------------------" << endl;
 for(int i=0; i<kiek; i++){
-    cout << setw(20) << std::left << A[i].v << std::left << setw(20) << A[i].p << setw(21) << std::right << A[i].galutinis << endl;
+    cout << setw(20) << std::left << A[i].v << std::left << setw(20) << A[i].p << setw(28) << std::right << A[i].galutinis << endl;
 }
 }
 
